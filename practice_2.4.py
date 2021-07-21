16e5bd06eb678187108fbafc66464e9a4324b9d2
Aleksey <87325003+xr0mogon@users.noreply.github.com>
2021-07-22 01:15:28 +0300

    Update practice_2.4.py
    remove unused code

--- practice_2.4.py
+++ practice_2.4.py
@@ -15,30 +15,4 @@ for word in my_str:
     else:
         print(f'{n}.{word}')
         n += 1
-
-#
-# for i in my_str:
-# #   print(i)
-#     if i.isalpha():
-#         word += i
-#     elif len(word) >= 10:  # Если слово длинное, выводить только первые 10 букв в слове.
-#         print(word[0:10])
-#     else:
-#         if word:
-#             print(f'{n+1}.(word)')
-#             word = ''
-
-#
-# while my_str[i] != split():
-
-# if (my_str[]) != ' ':
-
-
-
-
-# print('.\n'.join(my_string.split[0:10]))
-
-
-# n =
-# while n > 0:
-#     print(my_string[n])
+        
