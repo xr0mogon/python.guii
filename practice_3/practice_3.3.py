4865e21d279f6f4a1f9a1b0dc73a2d382824f898
Aleksey <87325003+xr0mogon@users.noreply.github.com>
2021-07-26 22:04:40 +0300

    Update practice_3.3.py
    * поменял на +

--- practice_3/practice_3.3.py
+++ practice_3/practice_3.3.py
@@ -7,6 +7,6 @@ def my_func():
         new_list.append(input(f'Введите {i+1} элемент списка: '))
     new_list.sort()
     print(new_list)
-    print(f'Сумма сумму наибольших двух аргументов: {int(new_list[1]) * int(new_list[2])}')
+    print(f'Сумма сумму наибольших двух аргументов: {int(new_list[1]) + int(new_list[2])}')
 
 my_func()
